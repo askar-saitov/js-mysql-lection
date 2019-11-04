ffeece6fcbd0ade9523b8253a68931aea8d9f954
Андрей Беляков <ttxiom@gmail.com>
2019-11-04 15:37:15 +0500

    Работа с CSV и JSON

--- utils.js
+++ utils.js
@@ -3,14 +3,14 @@ const conn = {
 	user: "gb_psis",
 	database: "gb_psis",
 	password: "ca8484adc89a"
-};
+}; // параметры для соединения с БД
 
 const menu = [
 	'0. Выйти из программы',
 	'1. Вывести данные',
 	'2. Сортировать по time',
 	'3. Сортировать по power'
-];
+]; // список пунктов меню
 
 let stop = function (connection) {
 	connection.end(() => console.log('___stop___'));
